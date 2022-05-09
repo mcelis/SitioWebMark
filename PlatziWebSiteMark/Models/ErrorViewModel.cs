c66e84643c6f4a2a0825e3b49d8c770405be72a2
Marco Celis <marco.celis@gmail.com>
2022-05-09 11:23:15 -0500

    Proyecto integrado

---
+++ PlatziWebSiteMark/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlatziWebSiteMark.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
